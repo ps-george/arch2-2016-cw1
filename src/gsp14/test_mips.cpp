6b98b91c5d2af77488643fc2d34815ba1965b5a2
George Punter <punter.s.g@gmail.com>
2016-10-21 15:53:27 +0100

    updated test code structure, tested that it compiles

--- src/gsp14/test_mips.cpp
+++ src/gsp14/test_mips.cpp
@@ -15,6 +15,9 @@
 #include <fstream>
 using namespace std;
 
+/*
+* MAP/STRUCT DEFINITIONS
+*/
 const std::map<std::string, uint32_t> r_to_op{
 	{"SLL",0x0},
 	{"SRL",0x02},
@@ -112,51 +115,129 @@ struct result_set{
 	string msg;
 	uint32_t ans;
 	uint32_t ans2;
-
 	result_set();
 	result_set(int passed);
 	result_set(int passed, string msg_in);
-
 };
+
 result_set::result_set(int passed_in, string msg_in){
 	msg = msg_in;
 	passed = passed_in;
 	ans = 0;
-	ans2=0;
+	ans2 = 0;
 }
 result_set::result_set(int passed_in){
 	msg = "";
 	passed = passed_in;
-	ans=0;
-	ans2=0;
+	ans = 0;
+	ans2 = 0;
 }
+/*
+* END OF DEFINITIONS
+*/
+/*
+* MAIN FUNCTION
+*/
+int main(int argc, char* argv[])
+{
+	/*! Print out arguments */
+    int i;
+    for(i=0; i<argc; ++i)
+    {
+    	printf("Argument %d : %s\n", i, argv[i]);
+    }
 
-void compare_model(mips_cpu_h cpu, model_state model, result_set &results){
-	mips_error err;
-	uint32_t tmp, pc;
-	for (unsigned i=0;i<32;i++){
-		err = mips_cpu_get_register(cpu, i, &tmp);
-		if (err!=mips_Success){
-			//results.msg << "Error reading from register " << i << endl;
-			results.passed=0;
-			return;
+    // Reverse map of opcodes for easy creation of opcodes.
+    for (auto const &i: op_to_str) {
+        	ij_to_op[i.second] = i.first;
+        }
+
+	// Create memory
+	mips_mem_h mem = mips_mem_create_ram(4096);
+
+	// Create cpu
+	mips_cpu_h cpu = mips_cpu_create(mem);
+
+	// Get & Set debug level
+	int debugLevel = set_debug_level(argc,argv,cpu);
+	// Prepare for tests
+	mips_test_begin_suite();
+	// Test that all 32 registers exist and contain uint32_t
+
+	// Read test_spec into vector of strings
+	vector<vector<string> > spec1;
+	parse_test_spec("mips_test_spec.csv", spec1);
+
+	// Execute test spec
+	run_spec(spec1, mem,cpu);
+	mips_test_end_suite();
+	return 0;
+}
+/*
+* END OF MAIN
+*/
+
+/*
+* GENERAL HELPER FUNCTIONS
+*/
+// Function which takes in a string naming the test file, tests all the cases in the file
+// PARSE TEST SPEC .CSV
+int parse_test_spec(string filename, vector<vector<string> > &spec){
+	string line;
+	ifstream test_file(filename);
+	// Check test file was opened.
+	if (test_file.is_open()){
+		cout << "Test file opened." << endl;
+	}
+	else{
+		cout << "Could not open test file." << endl;
+		return 1;
+	}
+
+	int i = 0;
+	while (getline(test_file,line)){
+		std::istringstream s(line);
+		std::string field;
+		vector<string> vec;
+		spec.push_back(vec);
+		while (getline(s, field,',')){
+			spec[i].push_back(field);
 		}
-		if (model.regs[i]!=tmp) {
-			//results.msg << "Register" << i << "is value" << tmp << ", should be value "
-			//<< model.regs[i] << endl;
-			results.passed=0;
+		i++;
+	}
+	test_file.close();
+	return 0;
+}
+// END OF PARSE TEST SPEC .csv
+// SET DEBUG LEVEL
+int set_debug_level(int argc, char* argv[],mips_cpu_h cpu){
+	unsigned level = 0;
+	FILE * dest;
+	const char* filename;
+	if (argc<3){
+		return 0;
+	}
+	if (!strcmp(argv[1],"-d")){
+		level = atoi(argv[2]);
+		dest = stdout;
+		if (argc>4&&(!strcmp(argv[3],"-f"))){
+			filename = argv[4];
+			if (!strcmp(filename,"stdout")){
+				dest = stdout;
+			}
+			else if (!strcmp(filename,"stderr")){
+				dest = stderr;
+			}
+			else{
+				dest = fopen(filename, "w");
+			}
 		}
 	}
-	err = mips_cpu_get_pc(cpu, &pc);
-	if (mips_test_check_err(err, results)){return;}
-	if (pc!=model.pc){
-		//results.msg << "PC is " << pc << ". Should be " << model.pc << "." << endl;
-		results.passed=0;
-		fprintf(stdout,"Was expecting answers 0x%x,0x%x\n",results.ans, results.ans2);
-	}
-	return;
+	cout << "Set debug level to: " << level << ". Print output to: " << filename;
+	mips_cpu_set_debug_level(cpu,level,dest);
+	return level;
 }
-
+// END OF SET DEBUG LEVEL
 
 /*!
  * Take in a list of params and function name and construct a bitstream for MIPS from it.
@@ -164,6 +245,7 @@ void compare_model(mips_cpu_h cpu, model_state model, result_set &results){
  * @param func - string containing the function name
  * @return
  */
+// CONSTRUCT BIT STREAM
 uint32_t test_construct_bitstream(string func,const vector<uint32_t> &params){
 	uint32_t bitstream=0x0;
 	uint32_t s,t,d,h,i,j;
@@ -195,7 +277,35 @@ uint32_t test_construct_bitstream(string func,const vector<uint32_t> &params){
 	bitstream = __builtin_bswap32(bitstream);
 	return bitstream;
 }
-
+// END OF CONSTRUCT bitstream
+// COMPARE MODEL
+void compare_model(mips_cpu_h cpu, model_state model, result_set &results){
+	mips_error err;
+	uint32_t tmp, pc;
+	for (unsigned i=0;i<32;i++){
+		err = mips_cpu_get_register(cpu, i, &tmp);
+		if (err!=mips_Success){
+			//results.msg << "Error reading from register " << i << endl;
+			results.passed=0;
+			return;
+		}
+		if (model.regs[i]!=tmp) {
+			//results.msg << "Register" << i << "is value" << tmp << ", should be value "
+			//<< model.regs[i] << endl;
+			results.passed=0;
+		}
+	}
+	err = mips_cpu_get_pc(cpu, &pc);
+	if (mips_test_check_err(err, results)){return;}
+	if (pc!=model.pc){
+		//results.msg << "PC is " << pc << ". Should be " << model.pc << "." << endl;
+		results.passed=0;
+		fprintf(stdout,"Was expecting answers 0x%x,0x%x\n",results.ans, results.ans2);
+	}
+	return;
+}
+// END OF COMPARE MODEL
+// MIPS TEST CHECK ERR
 int mips_test_check_err(mips_error err, result_set &results){
 	if (err!=mips_Success){
 		results.passed = 0;
@@ -204,156 +314,62 @@ int mips_test_check_err(mips_error err, result_set &results){
 	}
 	return 0;
 }
+// END OF MIPS TEST CHECK ERR
+/*
+* END OF GENERAL HELPER FUNCTIONS
+*/
+/*
+*	RUNNING THE TEST SPEC
+*/
+void run_spec(const vector<vector<string>> &spec, mips_mem_h mem, mips_cpu_h cpu){
+	// Iterate through spec and execute tests accordingly
+	uint32_t type;
+	string func = spec[0][1];
+	int testId;
 
-void test_i_type(const vector<string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu){
-	// initialise
-	uint32_t s,t,i,s_val, t_val, ans,loc, mem_loc;
-	string func, a_loc;
-	func = row[1];
-	s = s_to_ui(row[3]);
-	t = s_to_ui(row[4]);
-	i = s_to_ui(row[5]);
-	s_val = s_to_ui(row[6]);
-	t_val = s_to_ui(row[7]);
-	a_loc = row[8];
-	ans = s_to_ui(row[9]);
-	mem_loc = s_to_ui(row[10]);
-	loc = s_to_ui(row[11]);
-
-	model_state model;
-	// Set the value of the pc
-	mips_cpu_set_pc(cpu,loc);
-
-	// Set the value of the registers
-	mips_cpu_set_register(cpu,s,s_val);
-	mips_cpu_set_register(cpu,t,t_val);
-
-	// Create a model state with the correct values
-	if (a_loc=="regs"){
-		model_state model(cpu, t, ans,loc+4);
-	}
-	else if (a_loc=="j"){
-		model_state model(cpu,0,0,ans);
-	}
-
-	// make bitstream
-	vector<uint32_t> p{instr_I_type, s,t,i};
-	uint32_t bits = test_construct_bitstream(func, p);
-
-	// Write bitstream to memory location
-	mips_mem_write(mem, loc,4,(uint8_t*)&bits);
-
-	// Step the cpu
-	mips_error err = mips_cpu_step(cpu);
-
-	if (a_loc=="err"){
-		if(err!=ans){
-			results.passed=0;
+	for (uint i=0; i<spec.size();i++){
+		if (spec[i][0]=="Rtype"||spec[i][0]=="RTtype"||spec[i][0]=="Jtype"||spec[i][0]=="Itype"){
+			// Ignore header rows
+			cout << "ignore " << spec[i][0] << endl;
+			continue;
 		}
-		return;
-	}
+		type = s_to_ui(spec[i][0]);
+		// End the test
 
-	else if (a_loc=="mem"){
-		// Check mem loc for correct answer
-		uint32_t tmp;
-		mips_mem_read(mem, mem_loc, 4, (uint8_t*)&tmp);
-		tmp = __builtin_bswap32(tmp);
-		if (tmp!=ans){
-			results.passed=0;
+		// Reset the results
+		func = spec[i][1];
+		result_set results(1,spec[i][2]);
+		testId = mips_test_begin_test(func.c_str());
+
+		mips_cpu_reset(cpu);
+
+		switch(type){
+		case instr_R_type:
+			test_r_type(spec[i],results, mem, cpu);
+			break;
+		case instr_RT_type:
+			test_rt_type(spec[i],results,mem,cpu);
+			break;
+		case instr_J_type:
+			test_j_type(spec[i],results,mem,cpu);
+			break;
+		case instr_I_type:
+			test_i_type(spec[i],results,mem,cpu);
+			break;
 		}
+		if(!results.passed){cout << "Test " << testId << ", " << func <<". Testing " << results.msg << ". Result:" << results.passed << endl;}
+		mips_test_end_test(testId, results.passed, results.msg.c_str());
 	}
-
-	else {
-		compare_model(cpu,model,results);
-	}
-
 }
-
-void test_j_type(const vector<string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu){
-	// Initialise
-	uint32_t j,ans,loc;
-	string func, a_loc;
-	model_state model;
-	func = row[1];
-	j = s_to_ui(row[3]);
-	a_loc = row[4];
-	ans = s_to_ui(row[5]);
-	loc = s_to_ui(row[6]);
-	// Set the value of the pc (0)
-	mips_cpu_set_pc(cpu, loc);
-
-	// Create a model state with correct values
-	if (a_loc=="jal"){
-		model_state model(cpu,31,loc+8,ans);
-	}
-	else{
-		model_state model(cpu,0,0,ans);
-	}
-
-	// Make bitsteam
-	vector<uint32_t> p {instr_J_type,j};
-	uint32_t bits = test_construct_bitstream(func,p);
-	// Write bitstream to memory
-	mips_mem_write(mem, loc,4,(uint8_t*)&bits);
-
-	// Step the cpu
-	mips_error err = mips_cpu_step(cpu);
-
-	// Compare the model
-	if (a_loc=="err"){
-		if(err!=ans){
-			results.passed=0;
-		}
-		return;
-	}
-	compare_model(cpu,model,results);
-
-}
-
-void test_rt_type(const vector<string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu){
-	// Initialise
-	uint32_t s,i,s_val,loc;
-	int32_t ans;
-	string func, a_loc;
-	func = row[1];
-	s = s_to_ui(row[3]);
-	i = s_to_ui(row[4]);
-	s_val = s_to_ui(row[5]);
-	a_loc = row[6];
-	ans = s_to_i(row[7]);
-	loc = s_to_ui(row[8]);
-
-	// Write values to appropriate registers
-	mips_cpu_set_register(cpu,s,s_val);
-
-	// Set the value of the pc
-	mips_cpu_set_pc(cpu, loc);
-
-	// Create a model state with the correct dest value
-	model_state model(cpu,0,0,(int32_t)loc+ans);
-
-	// Make bitstream
-	vector<uint32_t> p {instr_RT_type,s,i};
-	uint32_t bits = test_construct_bitstream(func,p);
-
-	// Write bitstream to memory location
-	mips_mem_write(mem, loc, 4, (uint8_t*)&bits);
-
-	// Step the cpu
-	mips_error err = mips_cpu_step(cpu);
-
-	// Compare to model
-	if (a_loc =="err"){
-		if(err!=ans){
-			results.passed=0;
-		}
-		return;
-	}
-
-	compare_model(cpu, model, results);
-
-}
-
+// Could split into different functions which are more specific
+// Would have to define these categories myself, but for example: R-type normal, R-type branch, R-type MULT/DIV; I-type normal, I-type branch, I-type memory; J-type;
+// Use a_loc to define the type -> perhaps 0x10 normal R-type, 0x11 R-type branch etc, and this would define which bits of the .csv are used.
+// 0x20 I-type normal, 0x21 I-type branch, 0x22 I-type memory
+// And have a function which tests normal (r-type and i-type), a function which tests branches (R, RT, J and I type), a function which tests memory (I-type).
+// Could have helper functions which set the variables according to which type i.e. for I-type it will set s, t and i, and then construct the bitstream using I-type
+// This would mean a function for each type of testing. At the moment we have a function for each type of instruction, which means lots of if clauses to figure out
+// how to test that particular function
+// R-type Functions
 void test_r_type(const vector<string> &row,result_set &results, mips_mem_h mem, mips_cpu_h cpu){
 	// Initialise.
 	uint32_t s, s_val,t, t_val, h,d,ans,ans2,loc,d_val;
@@ -452,136 +468,152 @@ void test_r_type(const vector<string> &row,result_set &results, mips_mem_h mem, 
 	}
 	return;
 }
+// END OF R-type Functions
+// RT-type Functions
+void test_rt_type(const vector<string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu){
+	// Initialise
+	uint32_t s,i,s_val,loc;
+	int32_t ans;
+	string func, a_loc;
+	func = row[1];
+	s = s_to_ui(row[3]);
+	i = s_to_ui(row[4]);
+	s_val = s_to_ui(row[5]);
+	a_loc = row[6];
+	ans = s_to_i(row[7]);
+	loc = s_to_ui(row[8]);
 
-void run_spec(const vector<vector<string>> &spec, mips_mem_h mem, mips_cpu_h cpu){
-	// Iterate through spec and execute tests accordingly
-	uint32_t type;
-	string func = spec[0][1];
-	int testId;
+	// Write values to appropriate registers
+	mips_cpu_set_register(cpu,s,s_val);
 
-	for (uint i=0; i<spec.size();i++){
-		if (spec[i][0]=="Rtype"||spec[i][0]=="RTtype"||spec[i][0]=="Jtype"||spec[i][0]=="Itype"){
-			// Ignore header rows
-			cout << "ignore " << spec[i][0] << endl;
-			continue;
+	// Set the value of the pc
+	mips_cpu_set_pc(cpu, loc);
+
+	// Create a model state with the correct dest value
+	model_state model(cpu,0,0,(int32_t)loc+ans);
+
+	// Make bitstream
+	vector<uint32_t> p {instr_RT_type,s,i};
+	uint32_t bits = test_construct_bitstream(func,p);
+
+	// Write bitstream to memory location
+	mips_mem_write(mem, loc, 4, (uint8_t*)&bits);
+
+	// Step the cpu
+	mips_error err = mips_cpu_step(cpu);
+
+	// Compare to model
+	if (a_loc =="err"){
+		if(err!=ans){
+			results.passed=0;
 		}
-		type = s_to_ui(spec[i][0]);
-		// End the test
-
-		// Reset the results
-		func = spec[i][1];
-		result_set results(1,spec[i][2]);
-		testId = mips_test_begin_test(func.c_str());
-
-		mips_cpu_reset(cpu);
-
-		switch(type){
-		case instr_R_type:
-			test_r_type(spec[i],results, mem, cpu);
-			break;
-		case instr_RT_type:
-			test_rt_type(spec[i],results,mem,cpu);
-			break;
-		case instr_J_type:
-			test_j_type(spec[i],results,mem,cpu);
-			break;
-		case instr_I_type:
-			test_i_type(spec[i],results,mem,cpu);
-			break;
-		}
-		if(!results.passed){cout << "Test " << testId << ", " << func <<". Testing " << results.msg << ". Result:" << results.passed << endl;}
-		mips_test_end_test(testId, results.passed, results.msg.c_str());
+		return;
 	}
+	compare_model(cpu, model, results);
 }
+// END OF RT-type functions
+// J-type functions
+void test_j_type(const vector<string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu){
+	// Initialise
+	uint32_t j,ans,loc;
+	string func, a_loc;
+	model_state model;
+	func = row[1];
+	j = s_to_ui(row[3]);
+	a_loc = row[4];
+	ans = s_to_ui(row[5]);
+	loc = s_to_ui(row[6]);
+	// Set the value of the pc (0)
+	mips_cpu_set_pc(cpu, loc);
 
-int main(int argc, char* argv[])
-{
-	/*! Print out arguments */
-    int i;
-    for(i=0; i<argc; ++i)
-    {
-    	printf("Argument %d : %s\n", i, argv[i]);
-    }
-
-    // Reverse map of opcodes for easy creation of opcodes.
-    for (auto const &i: op_to_str) {
-        	ij_to_op[i.second] = i.first;
-        }
-
-	// Create memory
-	mips_mem_h mem = mips_mem_create_ram(4096);
-
-	// Create cpu
-	mips_cpu_h cpu = mips_cpu_create(mem);
-
-	// Get & Set debug level
-	int debugLevel = set_debug_level(argc,argv,cpu);
-	// Prepare for tests
-	mips_test_begin_suite();
-	// Test that all 32 registers exist and contain uint32_t
-
-	// Read test_spec into vector of strings
-	vector<vector<string> > spec1;
-	parse_test_spec("mips_test_spec.csv", spec1);
-
-	// Execute test spec
-	run_spec(spec1, mem,cpu);
-	mips_test_end_suite();
-	return 0;
-}
-
-// Function which takes in a string naming the test file, tests all the cases in the file
-int parse_test_spec(string filename, vector<vector<string> > &spec){
-	string line;
-	ifstream test_file(filename);
-	// Check test file was opened.
-	if (test_file.is_open()){
-		cout << "Test file opened." << endl;
+	// Create a model state with correct values
+	if (a_loc=="jal"){
+		model_state model(cpu,31,loc+8,ans);
 	}
 	else{
-		cout << "Could not open test file." << endl;
-		return 1;
+		model_state model(cpu,0,0,ans);
 	}
 
-	int i = 0;
-	while (getline(test_file,line)){
-		std::istringstream s(line);
-		std::string field;
-		vector<string> vec;
-		spec.push_back(vec);
-		while (getline(s, field,',')){
-			spec[i].push_back(field);
-		}
-		i++;
-	}
-	test_file.close();
-	return 0;
-}
+	// Make bitsteam
+	vector<uint32_t> p {instr_J_type,j};
+	uint32_t bits = test_construct_bitstream(func,p);
+	// Write bitstream to memory
+	mips_mem_write(mem, loc,4,(uint8_t*)&bits);
 
-int set_debug_level(int argc, char* argv[],mips_cpu_h cpu){
-	unsigned level = 0;
-	FILE * dest;
-	const char* filename;
-	if (argc<3){
-		return 0;
+	// Step the cpu
+	mips_error err = mips_cpu_step(cpu);
+
+	// Compare the model
+	if (a_loc=="err"){
+		if(err!=ans){
+			results.passed=0;
+		}
+		return;
 	}
-	if (!strcmp(argv[1],"-d")){
-		level = atoi(argv[2]);
-		dest = stdout;
-		if (argc>4&&(!strcmp(argv[3],"-f"))){
-			filename = argv[4];
-			if (!strcmp(filename,"stdout")){
-				dest = stdout;
-			}
-			else if (!strcmp(filename,"stderr")){
-				dest = stderr;
-			}
-			else{
-				dest = fopen(filename, "w");
-			}
+	compare_model(cpu,model,results);
+}
+// END OF J-type Functions
+// I-type Functions
+void test_i_type(const vector<string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu){
+	// initialise
+	uint32_t s,t,i,s_val, t_val, ans,loc, mem_loc;
+	string func, a_loc;
+	func = row[1];
+	s = s_to_ui(row[3]);
+	t = s_to_ui(row[4]);
+	i = s_to_ui(row[5]);
+	s_val = s_to_ui(row[6]);
+	t_val = s_to_ui(row[7]);
+	a_loc = row[8];
+	ans = s_to_ui(row[9]);
+	mem_loc = s_to_ui(row[10]);
+	loc = s_to_ui(row[11]);
+
+	model_state model;
+	// Set the value of the pc
+	mips_cpu_set_pc(cpu,loc);
+
+	// Set the value of the registers
+	mips_cpu_set_register(cpu,s,s_val);
+	mips_cpu_set_register(cpu,t,t_val);
+
+	// Create a model state with the correct values
+	if (a_loc=="regs"){
+		model_state model(cpu, t, ans,loc+4);
+	}
+	else if (a_loc=="j"){
+		model_state model(cpu,0,0,ans);
+	}
+
+	// make bitstream
+	vector<uint32_t> p{instr_I_type, s,t,i};
+	uint32_t bits = test_construct_bitstream(func, p);
+
+	// Write bitstream to memory location
+	mips_mem_write(mem, loc,4,(uint8_t*)&bits);
+
+	// Step the cpu
+	mips_error err = mips_cpu_step(cpu);
+
+	if (a_loc=="err"){
+		if(err!=ans){
+			results.passed=0;
+		}
+		return;
+	}
+
+	else if (a_loc=="mem"){
+		// Check mem loc for correct answer
+		uint32_t tmp;
+		mips_mem_read(mem, mem_loc, 4, (uint8_t*)&tmp);
+		tmp = __builtin_bswap32(tmp);
+		if (tmp!=ans){
+			results.passed=0;
 		}
 	}
-	cout << "Set debug level to: " << level << ". Print output to: " << filename;
-	mips_cpu_set_debug_level(cpu,level,dest);
-	return level;
+
+	else {
+		compare_model(cpu,model,results);
+	}
 }
+// END OF I-type functions
--- src/gsp14/test_mips.h
+++ src/gsp14/test_mips.h
@@ -17,53 +17,37 @@
  * specific test attempted
  */
 struct result_set;
-
-/*!
- *
- * @param rfunc - String (function name)
- * @param src1 - Source 1 (register, length 5)
- * @param src2 - Source 2 (register, length 5)
- * @param dest - Destination (register, length 5)
- * @return 32 bitstream correctly formatted (big-endian) for writing to memory.
- */
-uint32_t test_construct_R_bitstream(std::string rfunc, uint32_t src1, uint32_t src2, uint32_t dest);
-
-int set_debug_level(int argc, char* argv[], mips_cpu_h cpu);
-
-int parse_test_spec(std::string filename, std::vector<std::vector<std::string> > &spec);
-
-
-/*!
- * Writes a function into memory.
- * @param mem - Memory handler (needed because don't know internals of CPU to access memory)
- * @param loc - Need to ensure PC==loc to ensure the instruction is executed.
- * @param func - String of R func to write to memory
- * @param reg1 - index of reg1
- * @param reg2 - index of reg2
- * @param shift_amt - shift amount (if shifting)
- * @param dest - index of destination register
- * @return
- */
-mips_error write_R_func(
-		mips_mem_h mem,
-		uint32_t loc,
-		std::string func,
-		uint32_t reg1,
-		uint32_t reg2,
-		uint32_t shift_amt,
-		uint32_t dest);
+struct model_state;
 
 typedef enum _instr_type{
-    instr_R_type =0,
+  instr_R_type =0,
 	instr_RT_type = 0x1,
 	instr_J_type = 0x2,
 	instr_I_type = 0x3
 }instr_type;
 
-int mips_test_check_err(mips_error err, result_set &results);
-
 uint32_t s_to_ui(std::string s){return (uint32_t)strtol(s.c_str(),NULL,0);}
 int32_t s_to_i(std::string s){return (int32_t)strtol(s.c_str(),NULL,0);}
+
+int parse_test_spec(std::string filename, std::vector<std::vector<std::string> > &spec);
+
+int set_debug_level(int argc, char* argv[], mips_cpu_h cpu);
+
+uint32_t test_construct_bitstream(std::string func,const std::vector<uint32_t> &params);
+
+void compare_model(mips_cpu_h cpu, model_state model, result_set &results);
+
+int mips_test_check_err(mips_error err, result_set &results);
+
+void run_spec(const std::vector<std::vector<std::string>> &spec, mips_mem_h mem, mips_cpu_h cpu);
+
+void test_r_type(const std::vector<std::string> &row,result_set &results, mips_mem_h mem, mips_cpu_h cpu);
+
+void test_rt_type(const std::vector<std::string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu);
+
+void test_j_type(const std::vector<std::string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu);
+
+void test_i_type(const std::vector<std::string> &row, result_set &results, mips_mem_h mem, mips_cpu_h cpu);
 
 std::map<std::string, uint32_t> ij_to_op;
 
